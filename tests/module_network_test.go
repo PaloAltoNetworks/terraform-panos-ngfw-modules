8c5d98eb2699777b525c5d53da1f69118a3480d3
Sebastian Czech <sczech@paloaltonetworks.com>
2022-09-16 12:06:29 +0200

    Improve comparing maps

--- tests/module_network_test.go
+++ tests/module_network_test.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/logger"
@@ -23,52 +22,41 @@ func TestOutputsForCsvBasicExampleAndForVmSeries(t *testing.T) {
 	})
 	expected_zone := []string{"Trust-L3", "Untrust-L3", "external", "internal", "mgmt", "vpn"}
 	expected_zone_entry := map[string]interface{}{
-		"ethernet1/1": map[string]string{
+		"ethernet1/1": map[string]interface{}{
 			"mode": "layer3",
 			"vsys": "vsys1",
 		},
-		"ethernet1/2": map[string]string{
+		"ethernet1/2": map[string]interface{}{
 			"mode": "layer3",
 			"vsys": "vsys1",
 		},
-		"ethernet1/3": map[string]string{
+		"ethernet1/3": map[string]interface{}{
 			"mode": "layer3",
 			"vsys": "vsys1",
 		},
-		"ethernet1/4": map[string]string{
+		"ethernet1/4": map[string]interface{}{
 			"mode": "layer3",
 			"vsys": "vsys1",
 		},
-		"loopback.42": map[string]string{
+		"loopback.42": map[string]interface{}{
 			"mode": "layer3",
 			"vsys": "vsys1",
 		},
-		"tunnel.42": map[string]string{
+		"tunnel.42": map[string]interface{}{
 			"mode": "layer3",
 			"vsys": "vsys1",
 		},
 	}
-	defer terraform.Destroy(t, terraformOptions)
+	// defer terraform.Destroy(t, terraformOptions)
 
 	// when
 	terraform.InitAndApply(t, terraformOptions)
 	actual_zones := terraform.OutputList(t, terraformOptions, "panos_zones")
-	actual_zone_entry := terraform.OutputMap(t, terraformOptions, "panos_zone_entry")
+	actual_zone_entry := terraform.OutputMapOfObjects(t, terraformOptions, "panos_zone_entry")
 
 	// then
 	assert.Equal(t, expected_zone, actual_zones)
-	/*
-		while trying to compare using below options:
-		   assert.Equal(t, expected_zone_entry, actual_zone_entry)
-		or
-		   assert.True(t, reflect.DeepEqual(expected_zone_entry, actual_zone_entry))
-		there were problems with types of embedded map, so that's why below code to compare
-		strings created from maps for every key was used
-	*/
-	assert.Equal(t, len(expected_zone), len(actual_zone_entry))
-	for key := range actual_zone_entry {
-		assert.Equal(t, fmt.Sprint(expected_zone_entry[key]), fmt.Sprint(actual_zone_entry[key]))
-	}
+	assert.Equal(t, expected_zone_entry, actual_zone_entry)
 }
 
 /*
