b59f8a848276968704a808eb923858025f96b73c
Sebastian Czech <sczech@paloaltonetworks.com>
2022-09-21 09:50:45 +0200

    Store Terraform state in S3 bucket

--- examples/csv_examples/csv_basic_example/versions.tf
+++ examples/csv_examples/csv_basic_example/versions.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 0.13"
 
+  backend "s3" {
+    bucket = "hcf-terraform-tf-state"
+    key    = "csv-vmseries-terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     panos = {
       source  = "PaloAltoNetworks/panos"
