121fccc5305effc158e1bec8a27b34fefa99507d
Sebastian Czech <sczech@paloaltonetworks.com>
2022-09-29 14:27:06 +0200

    Fix order of policy and NAT

--- modules/policy/main.tf
+++ modules/policy/main.tf
@@ -13,7 +13,7 @@ resource "panos_security_rule_group" "this" {
   position_reference = try(each.value.position_reference, null)
 
   dynamic "rule" {
-    for_each = { for policy in each.value.rules : policy.name => policy }
+    for_each = [ for policy in each.value.rules : policy ]
     content {
       applications                       = try(rule.value.applications, ["any"])
       categories                         = try(rule.value.categories, ["any"])
@@ -74,7 +74,7 @@ resource "panos_panorama_nat_rule_group" "panorama_mode" {
 
 
   dynamic "rule" {
-    for_each = { for policy in each.value.rules : policy.name => policy if !can(policy.target) }
+    for_each = [ for policy in each.value.rules : policy if !can(policy.target) ]
 
     content {
       name        = rule.value.name
@@ -201,136 +201,7 @@ resource "panos_nat_rule_group" "fw_mode" {
 
 
   dynamic "rule" {
-    for_each = { for policy in each.value.rules : policy.name => policy if !can(policy.target) }
-
-    content {
-      name        = rule.value.name
-      description = try(rule.value.description, null)
-      tags        = try(rule.value.tags, null)
-      type        = try(rule.value.type, "ipv4")
-      disabled    = try(rule.value.disabled, false)
-
-      dynamic "target" {
-        for_each = can(rule.value.target) ? { for t in rule.value.target : t.serial => t } : {}
-
-        content {
-          serial    = lookup(target.value, "serial", null)
-          vsys_list = lookup(target.value, "vsys_list", null)
-        }
-      }
-
-      negate_target = try(rule.value.negate_target, false)
-
-
-      original_packet {
-        destination_addresses = try(rule.value.original_packet.destination_addresses, ["any"])
-        destination_zone      = try(rule.value.original_packet.destination_zone, "any")
-        source_addresses      = try(rule.value.original_packet.source_addresses, ["any"])
-        source_zones          = try(rule.value.original_packet.source_zones, ["any"])
-        service               = try(rule.value.original_packet.service, "any")
-      }
-
-      translated_packet {
-        destination {
-
-          dynamic "static_translation" {
-            for_each = rule.value.translated_packet.destination == "static_translation" ? [1] : []
-            content {
-              address = try(rule.value.translated_packet.static_translation.address, null)
-              port    = try(rule.value.translated_packet.static_translation.port, null)
-            }
-          }
-
-          dynamic "dynamic_translation" {
-            for_each = rule.value.translated_packet.destination == "dynamic_translation" ? [1] : []
-            content {
-              address      = try(rule.value.translated_packet.dynamic_translation.address, null)
-              port         = try(rule.value.translated_packet.dynamic_translation.port, null)
-              distribution = try(rule.value.translated_packet.dynamic_translation.distribution, null)
-            }
-          }
-        }
-
-        source {
-          dynamic "dynamic_ip_and_port" {
-            for_each = rule.value.translated_packet.source == "dynamic_ip_and_port" ? [1] : []
-            content {
-
-              dynamic "translated_address" {
-                for_each = length(try(rule.value.translated_packet.translated_addresses, {})) != 0 ? [1] : []
-                content {
-                  translated_addresses = try(rule.value.translated_packet.translated_addresses, null)
-                }
-              }
-
-              dynamic "interface_address" {
-                for_each = length(try(rule.value.translated_packet.interface_address, {})) != 0 ? [1] : []
-                content {
-                  interface  = try(rule.value.translated_packet.interface_address.interface, null)
-                  ip_address = try(rule.value.translated_packet.interface_address.ip_address, null)
-                }
-              }
-            }
-          }
-
-          dynamic "dynamic_ip" {
-            for_each = rule.value.translated_packet.source == "dynamic_ip" ? [1] : []
-            content {
-              translated_addresses = try(rule.value.translated_packet.translated_addresses, [])
-
-              dynamic "fallback" {
-                for_each = try(rule.value.translated_packet.fallback, null) != null ? [1] : []
-
-                content {
-                  dynamic "translated_address" {
-                    for_each = try(rule.value.translated_packet.fallback.translated_addresses, null) != null ? [1] : []
-                    content {
-                      translated_addresses = try(rule.value.translated_packet.fallback.translated_addresses, null)
-                    }
-                  }
-
-                  dynamic "interface_address" {
-                    for_each = try(rule.value.translated_packet.fallback.interface_address, null) != null ? [1] : []
-                    content {
-                      interface  = try(rule.value.translated_packet.fallback.interface_address.interface, null)
-                      type       = try(rule.value.translated_packet.fallback.interface_address.type, "ip")
-                      ip_address = try(rule.value.translated_packet.fallback.interface_address.ip_address, null)
-                    }
-                  }
-                }
-              }
-            }
-          }
-
-          dynamic "static_ip" {
-            for_each = rule.value.translated_packet.source == "static_ip" ? [1] : []
-            content {
-              translated_address = try(rule.value.translated_packet.static_ip.translated_address, null)
-              bi_directional     = try(rule.value.translated_packet.static_ip.bi_directional, false)
-            }
-          }
-        }
-      }
-    }
-  }
-}
-
-
-resource "panos_nat_rule_group" "this" {
-  for_each = var.panorama_mode == false && length(var.nat_policy) != 0 ? {
-    for item in var.nat_policy :
-    "${try(item.device_group, "shared")}_${replace(try(item.rulebase, "pre-rulebase"), "-", "_")}_${replace(try(item.position_keyword, ""), " ", "_")}"
-    => item
-  } : {}
-
-  #device_group       = try(each.value.device_group, "shared")
-  vsys               = try(each.value.vsys, "vsys1")
-  position_keyword   = try(each.value.position_keyword, "")
-  position_reference = try(each.value.position_reference, null)
-
-
-  dynamic "rule" {
-    for_each = { for policy in each.value.rules : policy.name => policy if !can(policy.target) }
+    for_each = [ for policy in each.value.rules : policy if !can(policy.target) ]
 
     content {
       name        = rule.value.name
