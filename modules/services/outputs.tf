85f7341b5de6ea52214d5c23829485510a25a063
Sebastian Czech <sczech@paloaltonetworks.com>
2023-05-29 16:11:03 +0200

    Add missing outputs for modules

--- modules/services/outputs.tf
+++ modules/services/outputs.tf
@@ -2,6 +2,14 @@ output "panorama_service_groups" {
   value = panos_panorama_service_group.this
 }
 
+output "service_groups" {
+  value = panos_service_group.this
+}
+
 output "panorama_services" {
   value = panos_panorama_service_object.this
+}
+
+output "services" {
+  value = panos_service_object.this
 }
